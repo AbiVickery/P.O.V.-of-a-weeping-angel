19712d37e7b7d3cbcc5046c0ebb502707d616e93
Abi Vickery <vickeryabi@gmail.com>
2020-02-03 12:47:27 -0600

    js

--- js/app.js
+++ js/app.js
@@ -13,5 +13,77 @@ function scenario1() {
         default:
             answer = "Either answer with A or B. Please try again.";
     }
-    document.getElementById("story1").innerHTML = answer;
+    document.getElementById("c1").innerHTML = answer;
+}
+
+function scenario2() {
+    var answer;
+    var story = prompt(".");
+    switch(story) {
+        case "A" :
+            answer = ".";
+            console.log('answer 1/2');
+        break;
+        case "B" :
+            answer = ".";
+            console.log('answer 2/2');
+        break;
+        default:
+            answer = "Either answer with 'A' or 'B'. Please try again.";
+    }
+    document.getElementById("c2").innerHTML = answer;
+}
+
+function scenario3() {
+    var answer;
+    var story = prompt(".");
+    switch(story) {
+        case "A" :
+            answer = ".";
+            console.log('answer 1/2');
+        break;
+        case "B" :
+            answer = ".";
+            console.log('answer 2/2');
+        break;
+        default:
+            answer = "Either answer with 'A' or 'B'. Please try again.";
+    }
+    document.getElementById("c3").innerHTML = answer;
+}
+
+function scenario4() {
+    var answer;
+    var story = prompt(".");
+    switch(story) {
+        case "A" :
+            answer = ".";
+            console.log('answer 1/2');
+        break;
+        case "B" :
+            answer = ".";
+            console.log('answer 2/2');
+        break;
+        default:
+            answer = "Either answer with 'A' or 'B'. Please try again.";
+    }
+    document.getElementById("c4").innerHTML = answer;
+}
+
+function scenario5() {
+    var answer;
+    var story = promt(".");
+    switch(story) {
+        case "A" :
+            answer = ".";
+            console.log('answer 1/2');
+        break;
+        case "B" :
+            answer = ".";
+            console.log('answer 2/2');
+        break;
+        default:
+            answer = "Either answer with 'A' or 'B'. Please try again.";
+    }
+    document.getElementById("c5").innerHTML = answer;
 }
